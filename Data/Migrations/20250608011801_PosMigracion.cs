6882cf78fbaa0ee2d03e2187ab9603fa3e841777
eduardotr2810 <eduardotr2810mail.com>
2025-06-07 20:35:38 -0500

    comit2

---
+++ Data/Migrations/20250608011801_PosMigracion.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace practica4.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PosMigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "t_contacto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombres = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Direccion = table.Column<string>(type: "TEXT", nullable: false),
+                    Telefono = table.Column<string>(type: "TEXT", nullable: false),
+                    Asunto = table.Column<string>(type: "TEXT", nullable: false),
+                    Mensaje = table.Column<string>(type: "TEXT", nullable: false),
+                    Etiqueta = table.Column<string>(type: "TEXT", nullable: true),
+                    Puntuacion = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_t_contacto", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "t_contacto");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # practica4
+dotnet ef migrations add PosMigracion --context practica4.Data.ApplicationDbContext -o "C:\Users\edutr\OneDrive\Escritorio\pc4\practica4\Data\Migrations"
